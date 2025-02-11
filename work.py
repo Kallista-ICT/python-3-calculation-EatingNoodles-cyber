854c7d278880c1de86a05972fc50c2b8040ebea2
EatingNoodles-cyber <gadisannisarayhana@gmail.com>
2025-02-11 06:39:09 +0000

    Submit assignment ##

---
+++ work.py
@@ -0,0 +1,9 @@
+#adding instruction will help user
+distance = float(input("Enter the distance (in meters): "))
+time = float(input("Enter the time (in seconds): "))
+
+#formula of velocity
+velocity = distance / time
+
+#add and change to make a more detailed answer. 
+print(f"the velocity is {velocity:.2f} m/s. ")
